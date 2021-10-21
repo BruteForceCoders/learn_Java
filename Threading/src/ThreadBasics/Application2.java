482eab0428c77e07b08ee6b8c8a6b52864aceac0
aaron_tetracube <aaronpsam@gmail.com>
2021-10-21 20:08:48 +0530

    Thread class updated.

--- Threading/src/ThreadBasics/Application2.java
+++ Threading/src/ThreadBasics/Application2.java
@@ -5,14 +5,12 @@ public class Application2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Starting Thread1");
-		Task2 taskRunner1 = new Task2();
-		taskRunner1.setName("Thread1");
+		Task2 taskRunner1 = new Task2("Thread-A");
 		taskRunner1.start();
 		
 		
 		System.out.println("Starting Thread2");
-		Task2 taskRunner2 = new Task2();
-		taskRunner2.setName("Thread2");
+		Task2 taskRunner2 = new Task2("Thread-B");
 		taskRunner2.start();;
 		
 		
@@ -20,7 +18,18 @@ public class Application2 {
 
 }
 class Task2 extends Thread{
+	
+	public String name;
+	
+	
+	
+	public Task2(String name) {
+		super();
+		this.name = name;
+	}
+
 	public void run() {
+		Thread.currentThread().setName(this.name);
 		for(int i=0;i<10;i++) {
 			System.out.println("number: "+i+"-"+Thread.currentThread().getName());
 			try {
