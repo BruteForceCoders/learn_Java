8b9558795541f214d519e802276ddcd196aea60a
aaron_tetracube <aaronpsam@gmail.com>
2021-11-09 17:22:29 +0530

    implemented wait and notify methods.

--- Threading/src/ProducerConsumer/Producer.java
+++ Threading/src/ProducerConsumer/Producer.java
@@ -1,11 +1,39 @@
 package ProducerConsumer;
 
+import java.util.List;
+
 public class Producer implements Runnable {
+
+	List<Integer> questionList = null;
+	final int LIMIT = 5;
+	private int questionNo;
+
+	public Producer(List<Integer> questionList) {
+		super();
+		this.questionList = questionList;
+	}
+
+	public void readQuestion(int questionNo) throws InterruptedException {
+		synchronized (questionList) {
+
+			while (questionList.size() == LIMIT) {
+				System.out.println("Question have pilled up .. wait for answers");
+				questionList.wait();
+			}
+		}
+		
+		synchronized (questionList) {
+			
+				System.out.println("New Question :"+questionNo);
+				questionList.add(questionNo);
+				Thread.sleep(100);
+				questionList.notify();
+		}
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
 	}
 
 }
