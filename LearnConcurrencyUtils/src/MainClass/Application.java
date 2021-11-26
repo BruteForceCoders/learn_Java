79c56b7754a796af3f6923717747e0a1b4f39a17
aaron_tetracube <aaronpsam@gmail.com>
2021-11-27 00:20:32 +0530

    Producer and consumer class added.

--- LearnConcurrencyUtils/src/MainClass/Application.java
+++ LearnConcurrencyUtils/src/MainClass/Application.java
@@ -1,11 +1,16 @@
 package MainClass;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("the program starts running....");
+		BlockingQueue<Integer> questionQueue = new ArrayBlockingQueue<Integer>(5);
+		
 	}
 
 }
